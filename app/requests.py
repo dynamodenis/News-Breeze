5ef845418734a88244ba516fc8b13415d5d76a68
Dynamo Denis <dmbugua66@gmail.com>
2020-04-24 17:16:58 +0300

    in the request we made an api request for sources

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,7 @@
 from app import app
+import urllib.request,json
+from models import Source
+
 
 #import API key
 api_key=app.config['API_KEY']
@@ -6,3 +9,18 @@ api_key=app.config['API_KEY']
 #import urls
 highlights_url=app.config['HEADLINES_API_URL']
 sources_url=app.config['SOURCE_API_URL']
+
+def get_source():
+    source_api_url=sources_url.format(api_key)
+
+    with urllib.request.urlopen(source_api_url) as url:
+        unread_data=url.read()
+        read_json=json.loads(unread_data)
+
+        source_results=[]
+
+        if read_json['sources']:
+            sources_list=read_json['sources']
+            source_results=process_results(sources_list)
+
+        return source_results
