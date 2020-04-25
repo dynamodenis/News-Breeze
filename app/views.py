5ba20afa4420660189d8f53585b94e5765dd4722
Dynamo Denis <dmbugua66@gmail.com>
2020-04-25 18:41:59 +0300

    ceated a new sports htmlfile

--- app/views.py
+++ app/views.py
@@ -7,13 +7,14 @@ def index():
     articles=get_article()
     sport=search_for_article('sports')
     business=search_for_article('business')
-    return render_template('index.html',articles=articles,sports=sport,business=business)
-
-
-
-@app.route('/sources')
-def sources():
     sources=get_source()
-    return render_template('source.html',source=sources)
+    return render_template('index.html',articles=articles,sports=sport,business=business,sources=sources)
+
+
+
+@app.route('/sports')
+def sources():
+    sport=search_for_article('sports')
+    return render_template('sports.html',sports=sport)
 
 
