44abb5571f26a8ca00b630b973f54fe99e36d64e
Dynamo Denis <dmbugua66@gmail.com>
2020-04-26 09:30:48 +0300

    created a request to server for news or articles

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template,request,url_for,redirect
 from .request import get_source,get_article,search_for_article
 
 @app.route('/')
@@ -8,8 +8,15 @@ def index():
     sport=search_for_article('sports')
     business=search_for_article('business')
     sources=get_source()
-    return render_template('index.html',articles=articles,sports=sport,business=business,sources=sources)
+    #Make request to get article from server
+    search=request.args.get('search_name')
 
+    if search:
+        return redirect(url_for('search',article_name=search))
+    else:
+        return render_template('index.html',articles=articles,sports=sport,business=business,sources=sources)
+
+    
 
 
 @app.route('/sports')
@@ -23,11 +30,11 @@ def business():
     business=search_for_article('business')
     return render_template('business.html',business=business)
 
-@app.route('/search/<artcle_name>')
-def search(artcle_name):
-    artcle_name_list=artcle_name.split(" ")
+@app.route('/search/<article_name>')
+def search(article_name):
+    artcle_name_list=article_name.split(" ")
     artcle_name_format="+".join(artcle_name_list)
     searched_article=search_for_article(artcle_name_format)
-    title=f'{artcle_name}'
+    title=f'{article_name}'
 
     return render_template('search.html',title=title,article=searched_article)
