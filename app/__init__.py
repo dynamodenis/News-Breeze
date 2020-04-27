66ea9d8078946f9ac69b145085df6711c96bdbef
Dynamo Denis <dmbugua66@gmail.com>
2020-04-27 21:33:48 +0300

    tests working

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 
-# app = Flask(__name__,instance_relative_config=True)
 
-# app.config.from_object(DevConfig)
-# app.config.from_pyfile('config.py')
 bootstrap=Bootstrap()
 
 
